ec92317430c298a8e4ba38e248af8d6da3d92207
aistestonciute <72549610+aistestonciute@users.noreply.github.com>
2023-09-11 14:19:51 +0300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,25 @@
+export default {
+    ELEMENT_EVENT_TYPE_READ: "eventTypeRead",
+    ERROR_CLASS: "error",
+    DISPLAY_TYPES: {
+        NONE: "none",
+        BLOCK: "block"
+    },
+    SESSION_STORAGE_EVENTS_KEY: "events",
+    calendar,
+    EVENT_TYPES: {
+        MEETING: "meeting",
+        CALL: "call",
+        OUT_OF_OFFICE: "outOfOffice"
+    },
+    WEEKDAYS: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+}
+
